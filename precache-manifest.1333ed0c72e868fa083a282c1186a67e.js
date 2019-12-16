b76d442ec9bdf098b10db1125cbbe266f3a19b2b
diegocamy <53247032+diegocamy@users.noreply.github.com>
2019-12-16 01:22:38 -0300

    Updates

---
+++ precache-manifest.1333ed0c72e868fa083a282c1186a67e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4163a4cd269e02772778dc1441238aad",
+    "url": "/topcharadas/index.html"
+  },
+  {
+    "revision": "487120be0fb9240498d0",
+    "url": "/topcharadas/static/css/main.5236ebf0.chunk.css"
+  },
+  {
+    "revision": "f4e7128f53448fe83161",
+    "url": "/topcharadas/static/js/2.83f409c1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/topcharadas/static/js/2.83f409c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "487120be0fb9240498d0",
+    "url": "/topcharadas/static/js/main.93750508.chunk.js"
+  },
+  {
+    "revision": "d9c693a69dec12db5223",
+    "url": "/topcharadas/static/js/runtime-main.01eaf962.js"
+  }
+]);
